ba30ab9ef86a290c07aeb43db14d9042b31cbee7
ademide <ademideadeboye@gmail.com>
2024-09-20 17:40:31 +0100

    commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,64 +2,16 @@
 
 const request = require('request');
 
-// Make sure that a film ID is provided as an argument
-if (process.argv.length < 3) {
-  console.error('Please provide a Star Wars film ID as an argument.');
-  process.exit(1);
-}
-
-// Get the film data from the Star Wars API
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) {
-    console.error('Error fetching film data:', err);
-    return;
-  }
-
-  // Parse the response body
-  let parsedBody;
-  try {
-    parsedBody = JSON.parse(body);
-  } catch (parseErr) {
-    console.error('Error parsing response:', parseErr);
-    return;
-  }
-
-  // Extract the list of characters (actors)
-  const actors = parsedBody.characters;
-
-  // Check if actors is an array and has elements
-  if (!Array.isArray(actors) || actors.length === 0) {
-    console.error('No characters found for this film.');
-    return;
-  }
-
-  // Call the function to print the characters in order
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
-
-// Recursive function to request and print each character in order
-const exactOrder = (actors, index) => {
-  if (index === actors.length) return; // Base case: stop recursion
-
-  // Fetch the data for each actor
-  request(actors[index], function (err, res, body) {
-    if (err) {
-      console.error('Error fetching character data:', err);
-      return;
-    }
-
-    let character;
-    try {
-      character = JSON.parse(body);
-    } catch (parseErr) {
-      console.error('Error parsing character data:', parseErr);
-      return;
-    }
-
-    console.log(character.name); // Print the character's name
-
-    // Recursively fetch the next actor
-    exactOrder(actors, index + 1);
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
 };
-
